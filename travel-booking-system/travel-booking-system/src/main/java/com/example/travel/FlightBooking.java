d6cbe41c245116d2f8cb2348f04cfb56af5ec249
Guruviswath2377 <guruviswath23@gmail.com>
2025-07-31 01:05:16 +0530

    Add files via upload

---
+++ travel-booking-system/travel-booking-system/src/main/java/com/example/travel/FlightBooking.java
@@ -0,0 +1,7 @@
+package com.example.travel;
+
+public class FlightBooking {
+	public void book(String destination) {
+		System.out.println("Flight booked to: " + destination);
+	}
+}
---
+++ travel-booking-system/travel-booking-system/src/main/java/com/example/travel/TrainBooking.java
@@ -0,0 +1,8 @@
+package com.example.travel;
+
+public class TrainBooking {
+	public void book(String destination) {
+		System.out.println("Train booked to: " + destination);
+	}
+
+}
